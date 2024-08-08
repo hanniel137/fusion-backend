76a65aff3d9fb32b2bf027941e7802bdb4f72e1e
hanniel137 <hanniel137@gmail.com>
2024-08-08 20:39:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,169 @@ import express, { json } from "express"
 import cors from "cors"
 import mysql from "mysql2/promise"
 import 'dotenv/config'
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cookieParser())
+app.use(cors({credentials: true, origin: 'https://fusion-client.vercel.app/'}))
 
 const db = await mysql.createConnection({
     host:process.env.DB_HOST,
     port:process.env.DB_PORT,
     user:process.env.DB_USER,
     password:process.env.DB_PASS,
-    database:process.env.DB_DATABASE
+    database:process.env.DB_DATABASE_COMPS
+})
+
+const user = await mysql.createConnection({
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_DATABASE_USER
+})
+
+const secretKey1 = process.env.ACCESS_TOKEN_SECRET;
+const secretKey2 = process.env.REFRESH_TOKEN_SECRET;
+
+const authenticate = (req, res, next) => {
+    const accessToken = req.headers['Authorization'];
+    const refreshToken = req.cookies['refreshToken'];
+  
+    if (!accessToken && !refreshToken) {
+      return res.status(401).send('Access Denied. No token provided.');
+    }
+  
+    try {
+      const decoded = jwt.verify(accessToken, secretKey1);
+      req.user = decoded.user;
+      next();
+    } catch (error) {
+      if (!refreshToken) {
+        return res.status(401).send('Access Denied. No refresh token provided.');
+      }
+  
+      try {
+        const decoded = jwt.verify(refreshToken, secretKey2);
+        const accessToken = jwt.sign({ user: decoded.user }, secretKey1, { expiresIn: '20s' });
+  
+        res
+          .cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'strict' })
+          .header('Authorization', accessToken)
+          .send(decoded.user);
+      } catch (error) {
+        return res.status(400).send('Invalid Token.');
+      }
+    }
+  };
+
+app.post("/register", async(req,res)=>{
+    try {
+        const {firstName, lastName, number, email, password, retypePassword} = req.body;
+        if (!(firstName && lastName && number && email && password && retypePassword)){
+            res.status(401).send('All fields are mandatory!!');
+            return;
+        }
+        if (password !== retypePassword){
+            res.status(402).send('Password Mismatch!!');
+            return;
+        }
+        const [count, fields] = await user.query(
+            "SELECT COUNT(*) AS CNT FROM fg_user_data.user where number = "+number+" or email='"+email+"'"
+        );
+        if(count[0].CNT){
+            res.status(403).send('User Already Exists!!');
+            return;
+        } else {
+            var salt = bcrypt.genSaltSync(10);
+            var hashedPassword = bcrypt.hashSync(password, salt);
+            const [count, fields] = await user.query(
+                "INSERT INTO fg_user_data.user (firstname, lastname, number, email, password) VALUES ('"+req.body.firstName+"', '"+req.body.lastName+"', "+req.body.number+", '"+req.body.email+"', '"+hashedPassword+"')"
+            )
+            res.status(201).send("Sucess!! User Created!");
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+app.post("/login", async(req,res)=>{
+    try{
+        const {number, password} = req.body;
+        if (!(number && password)){
+            res.status(401).send("All fields are mandatory!");
+            return;
+        }
+        const [count, fields1] = await user.query(
+            "SELECT COUNT(*) AS CNT FROM fg_user_data.user where number='"+number+"'"
+        )
+        if(!(count[0].CNT)){
+            res.status(402).send('User does not exist!');
+            return;
+        } else {
+            const [userChk, fields2] = await user.query(
+                "SELECT password, firstname, number, email FROM fg_user_data.user where number='"+number+"'"
+            )
+            const passCheck = await bcrypt.compare(password, userChk[0].password);
+            if(passCheck){
+                const userInfo = {"firstName": userChk[0].firstname,
+                    "number" : userChk[0].number,
+                    "email" : userChk[0].email
+                }
+                const accessToken = jwt.sign({userInfo},
+                    secretKey1,
+                    { expiresIn: '60s'}
+                );
+                const refreshToken = jwt.sign({userInfo},
+                    secretKey2,
+                    { expiresIn: '3600s'}
+                );
+                res.cookie('refreshToken', refreshToken, { expires: new Date(new Date().getTime()+60*60*1000), httpOnly: true, secure: false}).header('Authorization', accessToken).send(userInfo);
+            } else {
+                res.status(403).send("Invalid Password!");
+                return;
+            }
+        }
+    }
+    catch(err){
+        console.log(err); 
+    }
+})
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.cookies['refreshToken'];
+    if (!refreshToken) {
+      return res.status(401).send('Access Denied. No refresh token provided.');
+    }
+  
+    try {
+      const decoded = jwt.verify(refreshToken, secretKey2);
+      const accessToken = jwt.sign({ user: decoded.user }, secretKey1, { expiresIn: '60s' });
+  
+      res
+        .header('Authorization', accessToken)
+        .send(decoded.userInfo);
+    } catch (error) {
+      return res.status(400).send('Invalid refresh token.');
+    }
+  });
+
+app.get("/logout", async(req, res)=> {
+    try {
+        res.clearCookie('jwt', { httpOnly: true, secure: false, path: '/' });
+        res.status(200).send("Logged Out!")
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 app.get("/", (req,res)=>{
-    res.json("Hello!", process.env.DB_HOST)
+    res.json("Hello!")
 })
 
 
@@ -26,7 +173,7 @@ app.get("/platform", async (req,res)=>{
         const [results, fields] = await db.query(
             "SELECT * FROM fg_component_data.processor where type='platform'"
         );
-        res.json(results);
+        res.json(results); 
         }
     catch (err) {
         console.log(err);
